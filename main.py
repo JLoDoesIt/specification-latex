a62d2a7025813dd5f8ec8ec5b8a1c5b4ed9db2d6
jlo <jcavazza@protonmail.com>
2021-02-25 16:18:36 +0100

    made the code a little bit more generic: column can be changed in the main function as well as title and authors

--- main.py
+++ main.py
@@ -3,39 +3,55 @@ from pylatex import Document, Section, Subsection, NewPage, Command
 from pylatex.utils import bold, NoEscape
 
 
-def create_latex_specification(i_epic_list):
+def create_latex_specification(
+        i_subsections_list,
+        i_section_column="Service",
+        i_sub_section_column="Epic",
+        i_sub_section_description_column="Description",
+        i_sub_section_characteristic_column="User",
+        i_title='Specification',
+        i_authors='Author NAMES'):
     geometry_options = {}
     doc = Document(geometry_options=geometry_options)
-    add_first_page(doc)
+    add_first_page(doc, i_title, i_authors)
     doc.append(NoEscape(r'\tableofcontents'))
     doc.append(NewPage())
-    i_epic_list.sort_values(by=['Service'], inplace=True)
-    a_service_list = []
-    for it_epic_row in i_epic_list.itertuples():
-        if not pd.isna(it_epic_row.Epic):
-            a_service = it_epic_row.Service if not pd.isna(it_epic_row.Service) else 'No service name'
-            an_epic = it_epic_row.Epic
-            a_description = it_epic_row.Description if not pd.isna(it_epic_row.Description) else 'No description'
-            a_user = it_epic_row.Utilisateur if not pd.isna(it_epic_row.Utilisateur) else 'No user'
+    i_subsections_list.sort_values(by=[i_section_column], inplace=True)
+    a_sections_list = []
+    for it_subsection_row in i_subsections_list.itertuples():
+        if not pd.isna(getattr(it_subsection_row, i_sub_section_column)):
+            a_section_name = get_column_content_for_row(i_section_column, it_subsection_row)
+            a_subsection_name = get_column_content_for_row(i_sub_section_column, it_subsection_row)
+            a_subsection_description = get_column_content_for_row(i_sub_section_description_column, it_subsection_row)
+            a_subsection_characteristic = get_column_content_for_row(i_sub_section_characteristic_column,
+                                                                     it_subsection_row)
             # print(an_epic + ' - ' + a_description)
-            if a_service not in a_service_list:
-                a_service_list.append(a_service)
-                with doc.create(Section(a_service)):
-                    create_epic_subsection(a_description, an_epic, doc, a_user)
+            if a_section_name not in a_sections_list:
+                a_sections_list.append(a_section_name)
+                with doc.create(Section(a_section_name)):
+                    create_subsection(a_subsection_description, a_subsection_name, doc,
+                                      i_sub_section_characteristic_column, a_subsection_characteristic)
             else:
-                create_epic_subsection(a_description, an_epic, doc, a_user)
+                create_subsection(a_subsection_description, a_subsection_name, doc, i_sub_section_characteristic_column,
+                                  a_subsection_characteristic)
     doc.generate_pdf('full', clean_tex=False)
 
 
-def create_epic_subsection(i_description, i_epic, io_doc, i_user):
-    with io_doc.create(Subsection(i_epic)):
-        io_doc.append(bold("User: " + i_user+"\n"))
-        io_doc.append(i_description)
+def get_column_content_for_row(i_column_name, i_row):
+    return getattr(i_row, i_column_name) if not pd.isna(
+        getattr(i_row, i_column_name)) else 'No ' + i_column_name
 
 
-def add_first_page(io_doc):
-    io_doc.preamble.append(Command('title', 'Ulysseus Digital Platform Specification'))
-    io_doc.preamble.append(Command('author', 'Candice OUDOT, Mathilde La Plaze, Jean-Loic CAVAZZA'))
+def create_subsection(i_subsection_description, i_subsection_name, io_doc, i_sub_section_characteristic_column,
+                      i_subsection_characteristic):
+    with io_doc.create(Subsection(i_subsection_name)):
+        io_doc.append(bold(i_sub_section_characteristic_column + ": " + i_subsection_characteristic + "\n"))
+        io_doc.append(i_subsection_description)
+
+
+def add_first_page(io_doc, i_title, i_authors):
+    io_doc.preamble.append(Command('title', i_title))
+    io_doc.preamble.append(Command('author', i_authors))
     io_doc.preamble.append(Command('date', NoEscape(r'\today')))
     io_doc.append(NoEscape(r'\maketitle'))
     io_doc.append(NewPage())
@@ -43,6 +59,8 @@ def add_first_page(io_doc):
 
 if __name__ == '__main__':
     df = pd.read_csv('specifications.csv', sep=';')
-    create_latex_specification(df)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    create_latex_specification(
+        df,
+        i_title='Ulysseus Digital Platform Specification',
+        i_authors='Candice OUDOT, Mathilde La Plaze, Jean-Loic CAVAZZA'
+    )
